423fe1bac74c2e9f0c71bd700582d690c787e099
Saransh Saini <saranshsss99@gmail.com>
2022-05-23 17:26:27 +0530

    searchbar updated

--- src/components/content/SearchBar.tsx
+++ src/components/content/SearchBar.tsx
@@ -1,5 +1,17 @@
+import { FaSearch } from "react-icons/fa";
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  return (
+    <div className="flex flex-col items-center gap-y-3 w-full sm:w-3/4 md:w-2/4 text-white">
+      <input
+        className="block w-full px-3 py-1.5 bg-transparent border-2 rounded-md focus:outline-none"
+        type="search"
+        placeholder="Search high resolution images"
+      />
+      <button className="bg-transparent border-[1.5px] rounded-md text-xl px-5 py-2">
+        <FaSearch />
+      </button>
+    </div>
+  );
 };
 
 export default SearchBar;
--- src/components/content/SearchBarCard.tsx
+++ src/components/content/SearchBarCard.tsx
@@ -1,12 +1,15 @@
+import SearchBar from "./SearchBar";
+
 const SearchBarCard = () => {
   return (
     <section className="bg-[url('/assets/mountain.jpg')] bg-auto bg-center bg-no-repeat px-10 min-h-[28rem] flex flex-col justify-center items-center bg-gray-600 bg-blend-overlay gap-y-5">
       <h1 className="text-white font-bold text-3xl text-center">
         Download High Quality Image by creators
       </h1>
-      <p className="text-gray-300">
+      <p className="text-gray-300 text-center">
         Over 2.4 million+ stock Image by our talented community
       </p>
+      <SearchBar />
     </section>
   );
 };
