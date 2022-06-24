188dd22e4da3d4175a0decff19a238244409782e
Saransh Saini <saranshsss99@gmail.com>
2022-06-24 19:18:47 +0530

    getting random photos on page load

--- src/components/images/index.tsx
+++ src/components/images/index.tsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Random } from "unsplash-js/dist/methods/photos/types";
+import unsplash from "../../api/unsplash";
 import { useAppContext } from "../../context/AppContext";
 import ImagesCol from "./ImagesCol";
 
 const Images = () => {
   const {
     state: { images },
+    dispatch,
   } = useAppContext();
+
+  const getRandomPhotos = async () => {
+    try {
+      const res = await unsplash.photos.getRandom({
+        count: 30,
+      });
+      if (res.response) {
+        let results: Random[] = [];
+        if (Array.isArray(res.response)) {
+          results = res.response;
+        } else {
+          results.push(res.response);
+        }
+        dispatch({ type: "ADD_IMAGES", payload: results });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getRandomPhotos();
+  }, []);
 
   return (
     <div className="p-5 md:py-10 md:px-20 grid gap-10 lg:grid-cols-2 2xl:grid-cols-4">
